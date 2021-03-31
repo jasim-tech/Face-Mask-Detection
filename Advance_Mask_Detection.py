31a08420c7da734eca997e9a64e543fd8edcce42
Mohammad jasim <70314488+jasim-tech@users.noreply.github.com>
2021-03-31 14:31:00 +0600

    Add files via upload

---
+++ Advance_Mask_Detection.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2,os
+
+data_path='D:\projects\Project\First project mask detector\train'
+categories=os.listdir(data_path)
+labels=[i for i in range(len(categories))]
+
+label_dict=dict(zip(categories,labels)) #empty dictionary
+
+print(label_dict)
+print(categories)
+print(labels)
+
+
+# In[2]:
+
+
+img_size=100
+data=[]
+target=[]
+
+
+for category in categories:
+    folder_path=os.path.join(data_path,category)
+    img_names=os.listdir(folder_path)
+        
+    for img_name in img_names:
+        img_path=os.path.join(folder_path,img_name)
+        img=cv2.imread(img_path)
+
+        try:
+            gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)           
+            #Coverting the image into gray scale
+            resized=cv2.resize(gray,(img_size,img_size))
+            #resizing the gray scale into 50x50, since we need a fixed common size for all the images in the dataset
+            data.append(resized)
+            target.append(label_dict[category])
+            #appending the image and the label(categorized) into the list (dataset)
+
+        except Exception as e:
+            print('Exception:',e)
+            #if any exception rasied, the exception will be printed here. And pass to the next image
+
+
+# In[3]:
+
+
+import numpy as np
+
+data=np.array(data)/255.0
+data=np.reshape(data,(data.shape[0],img_size,img_size,1))
+target=np.array(target)
+
+from keras.utils import np_utils
+
+new_target=np_utils.to_categorical(target)
+
+
+# In[4]:
+
+
+np.save('data',data)
+np.save('target',new_target)
+
+
+# In[5]:
+
+
+import numpy as np
+
+data=np.load('data.npy')
+target=np.load('target.npy')
+
+#loading the save numpy arrays in the previous code
+
+
+# In[6]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten,Dropout
+from keras.layers import Conv2D,MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+
+model=Sequential()
+
+model.add(Conv2D(200,(3,3),input_shape=data.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The first CNN layer followed by Relu and MaxPooling layers
+
+model.add(Conv2D(100,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The second convolution layer followed by Relu and MaxPooling layers
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+#Flatten layer to stack the output convolutions from second convolution layer
+model.add(Dense(50,activation='relu'))
+#Dense layer of 64 neurons
+model.add(Dense(2,activation='softmax'))
+#The Final layer with two outputs for two categories
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+
+# In[7]:
+
+
+from sklearn.model_selection import train_test_split
+
+train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.1)
+
+
+# In[1]:
+
+
+checkpoint = ModelCheckpoint('model-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+history=model.fit(train_data,train_target,epochs=4,callbacks=[checkpoint],validation_split=0.2)
+
+
+# In[9]:
+
+
+from keras.models import load_model
+import cv2
+import numpy as np
+
+
+# In[10]:
+
+
+#model = load_model('model-017.model')
+#face_clsfr = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#os.getcwd()
+#os.listdir()
+#face_clsfr=cv2.CascadeClassifier('C:\\Users\\AKSON MAHMUDUL\\Desktop\\mask detection\\haarcascade_frontalface_default.xml')
+
+#source=cv2.VideoCapture(2)
+
+labels_dict={1:'MASK',0:'NO MASK'}
+color_dict={0:(0,255,0),1:(0,0,255)}
+
+
+# In[14]:
+
+
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        
+         
+        
+    
+    
+        face_img=gray[y:y+w,x:x+w]
+        resized=cv2.resize(face_img,(100,100))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,100,100,1))
+        result=model.predict(reshaped)
+
+        label=np.argmax(result,axis=1)[0]
+      
+        cv2.rectangle(img,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(img,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(img, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+        
+        #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display
+    cv2.imshow('LIVE', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+        
+# Release the VideoCapture object
+cap.release()
+
+
+# In[ ]:
+
+
+
+
